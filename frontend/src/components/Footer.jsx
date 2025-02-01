a2d6cb59d82c7db9c2220233a94aa8ba96eaadc6
sha1sha2 <shashvatsen00@gmail.com>
2025-02-01 23:23:02 +0530

    footer styling

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="w-full bg-slate-200">
-      <div className="container mx-auto  text-black">
-        <p className="font-bold text-center">Made with ❣️ by Shashvat</p>
+      <div className="container mx-auto  p-4 text-black">
+        <p className="font-bold text-center">Dynamic Coding With Shashvat</p>
       </div>
     </div>
   );
